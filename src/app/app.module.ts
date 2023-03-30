7cf1e705ccd054b231cb3cdf9820e68e570f5538
Miko Barreyro <110441796+barreyrojohnmiko@users.noreply.github.com>
2023-03-30 13:52:32 +0800

    created dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent],
@@ -15,6 +16,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
       { path: '', component: DashboardComponent },
       { path: 'dashboard', component: DashboardComponent },
     ]),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-
+  numberFormat(x: number) {
+    return x.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
 }
