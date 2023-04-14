1e756b71c4b0edd22a13ade6f0a0364a48f55e2e
Miko Barreyro <110441796+barreyrojohnmiko@users.noreply.github.com>
2023-04-14 16:57:29 +0800

    added primeng carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { SidebarComponent } from './views/sidebar/sidebar.component';
 
+import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, LoginComponent, SidebarComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SidebarComponent,
+    DashboardComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    CarouselModule,
+    ButtonModule,
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
       { path: '', component: DashboardComponent },
       { path: 'dashboard', component: DashboardComponent },
     ]),
-    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Carousel } from 'primeng/carousel';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  @ViewChild(Carousel) carousel!: Carousel;
+
   numberFormat(x: number) {
     return x.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+
+  images = [
+    'https://w.wallhaven.cc/full/1p/wallhaven-1ppld1.jpg',
+    'https://w.wallhaven.cc/full/m3/wallhaven-m3zjx1.jpg',
+  ];
+
+  prevSlide() {
+    (this.carousel as any).prev();
+  }
+  
+  nextSlide() {
+    (this.carousel as any).next();
   }
 }
